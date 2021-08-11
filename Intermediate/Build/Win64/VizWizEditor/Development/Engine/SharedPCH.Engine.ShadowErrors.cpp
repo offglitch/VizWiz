ca1138b0dbd24044bfc1bc76aebbe6c45bfd7cab
Majid Alturki <majidalturki@live.com>
2021-08-11 20:18:17 +0100

    update .git

--- Intermediate/Build/Win64/VizWizEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30040
-#include "C:/Users/Majid/Desktop/MSc/Labs/VizWiz/Intermediate/Build/Win64/VizWizEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Intermediate/Source/VizWiz/VizWiz.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "VizWiz.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VizWiz, "VizWiz" );
